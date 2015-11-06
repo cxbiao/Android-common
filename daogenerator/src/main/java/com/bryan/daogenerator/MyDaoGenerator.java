413d7af97fbfe957de6f6de7a39e5bfb222d2ca1
cxbiao <690158801@qq.com>
2015-11-06 09:35:25 +0800

    持续集成
    持续集成

--- daogenerator/src/main/java/com/bryan/daogenerator/MyDaoGenerator.java
+++ daogenerator/src/main/java/com/bryan/daogenerator/MyDaoGenerator.java
@@ -1,11 +1,14 @@
 package com.bryan.daogenerator;
 
+import java.io.File;
+
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 public class MyDaoGenerator {
 
+    private static final String desPath=new File("").getAbsolutePath()+"/app/src/main/java-gen";
     public static void main(String[] args) throws Exception {
         Schema schema = new Schema(1, "com.bryan.greendao");
         //Keep sections 防止覆盖
@@ -16,6 +19,6 @@ public class MyDaoGenerator {
         note.addStringProperty("comment");
         note.addDateProperty("date");
         //写绝对路径
-        new DaoGenerator().generateAll(schema, "../app/src/main/java-gen");
+        new DaoGenerator().generateAll(schema, desPath);
     }
 }
