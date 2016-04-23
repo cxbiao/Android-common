708e865eb9a8e3d5c1b4b76750097d8f917d328f
cxbiao <690158801@qq.com>
2016-04-23 17:17:24 +0800

    增加QQ视频的广告图样式

--- app/src/main/java/com/bryan/commondemo/fragment/AdvFragment.java
+++ app/src/main/java/com/bryan/commondemo/fragment/AdvFragment.java
@@ -2,17 +2,24 @@ package com.bryan.commondemo.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.bryan.commondemo.R;
 import com.bryan.commondemo.widget.AdvBanner;
+import com.bryan.commondemo.widget.ClipViewPager;
+import com.bryan.commondemo.widget.ScalePageTransformer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -26,8 +33,13 @@ public class AdvFragment extends BaseFragment {
     AdvBanner advBanner;
     @Bind(R.id.ll_pg)
     LinearLayout ll_Pg;
+    @Bind(R.id.viewpager)
+    ClipViewPager mViewPager;
+    @Bind(R.id.page_container)
+    RelativeLayout pageContainer;
 
     private Integer[] imgSrcs={R.mipmap.img1,R.mipmap.img2,R.mipmap.img3,R.mipmap.img4,R.mipmap.img5};
+    private MyAdapter mPagerAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,10 +52,46 @@ public class AdvFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setViewPages();
+        setAdvBanner();
+        setQQBanner();
     }
 
-    private void setViewPages() {
+    private void setQQBanner() {
+        mViewPager.setPageTransformer(true, new ScalePageTransformer());
+
+        pageContainer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return mViewPager.dispatchTouchEvent(event);
+            }
+        });
+
+        List<Integer> list = new ArrayList<>();
+        list.add(R.mipmap.img1);
+        list.add(R.mipmap.img2);
+        list.add(R.mipmap.img3);
+        list.add(R.mipmap.img4);
+        list.add(R.mipmap.img5);
+        list.add(R.mipmap.img6);
+        list.add(R.mipmap.img7);
+        list.add(R.mipmap.img8);
+
+        //设置OffscreenPageLimit
+        mViewPager.setOffscreenPageLimit(list.size());
+        mPagerAdapter = new MyAdapter(list);
+        mViewPager.setAdapter(mPagerAdapter);
+
+        //setCurrentItem会改变viewpager中child的索引，除非给child设置tag,用tag找child
+        mViewPager.setCurrentItem(list.size()/2);
+        mViewPager.setListener(new ClipViewPager.ViewClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(context,""+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void setAdvBanner() {
 
          advBanner.setSource(Arrays.asList(imgSrcs));
          for(int i=0;i<imgSrcs.length;i++){
@@ -66,9 +114,42 @@ public class AdvFragment extends BaseFragment {
     }
 
 
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+
+    public class MyAdapter extends PagerAdapter{
+
+        private List<Integer> list;
+        public MyAdapter(List<Integer> data){
+            list=data;
+        }
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView view=new ImageView(container.getContext());
+            view.setTag(position);
+            view.setImageResource(list.get(position));
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
     }
 }
---
+++ app/src/main/java/com/bryan/commondemo/widget/ClipViewPager.java
@@ -0,0 +1,110 @@
+package com.bryan.commondemo.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+/**
+ * Created by HanHailong on 15/9/27.
+ */
+public class ClipViewPager extends ViewPager {
+
+
+    private ViewClickListener listener;
+    public ClipViewPager(Context context) {
+        super(context);
+    }
+
+    public ClipViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ViewClickListener getListener() {
+        return listener;
+    }
+
+    public void setListener(ViewClickListener listener) {
+        this.listener = listener;
+    }
+
+
+    float downX;
+    float downY;
+    long downTime;
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()){
+            case  MotionEvent.ACTION_DOWN:
+                downX=ev.getX();
+                downY=ev.getY();
+                downTime = System.currentTimeMillis();
+                break;
+            case  MotionEvent.ACTION_UP:
+                int index = viewOfClickOnScreen(ev);
+                if (index != -1) {
+                    float upX =  ev.getX();
+                    float upY=ev.getY();
+                    long upTime = System.currentTimeMillis();
+                    if (Math.abs(upX - downX)< ViewConfiguration.get(getContext()).getScaledTouchSlop()
+                            &&Math.abs(upY - downY)< ViewConfiguration.get(getContext()).getScaledTouchSlop()
+                          &&upTime - downTime < 500) {
+                        if(listener!=null){
+                            listener.onClick(index);
+                        }
+                    }
+                    if (getCurrentItem() != index) {
+                        setCurrentItem(index);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * @param ev
+     * @return
+     */
+    private int viewOfClickOnScreen(MotionEvent ev) {
+        int childCount = getChildCount();
+        int[] location = new int[2];
+        int[] pageLocation=new int[2];
+        getLocationOnScreen(pageLocation);
+        for (int i = 0; i < childCount; i++) {
+            View v = findViewWithTag(i);
+            if(v==null) continue;
+            v.getLocationOnScreen(location);
+            int minX = location[0];
+            int minY = location[1];
+
+            int maxX = location[0] + v.getWidth();
+            int maxY = location[1]+ v.getHeight();
+
+            float x = ev.getRawX();
+            float y = ev.getRawY();
+
+            //如果坐标位于viewpager内部，则直接返回当前view,不再继续查找
+            if ((x >=pageLocation[0] && x <=pageLocation[0]+getWidth()) && (y >=pageLocation[1] && y <=pageLocation[1] +getHeight())) {
+                 return getCurrentItem();
+            }
+            //父控件传递来的事件
+            else if ((x > minX && x < maxX) && (y > minY && y < maxY)) {
+                  return i;
+            }
+
+
+        }
+        return -1;
+    }
+
+     public  interface ViewClickListener {
+         void onClick(int position);
+    }
+}
